4c3ba75989ac7f8e2e89fb4a9d7a36434aa3d8aa
Juanito3500 <juanchavezsaldana1@gmail.com>
2024-10-04 23:01:44 -0500

    Iniciando Menu de accecibilidad

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ViewTransitions } from "next-view-transitions";
-import AccesibilityBackground from "@/components/AccecibilityBackground";
 import Accesibility from "@/components/icons/Accesibility";
 
 const geistSans = localFont({
