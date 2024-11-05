9f39ca0003adb693a916cd0c2e9c6e1b4d7985b0
Juanito3500 <juanchavezsaldana1@gmail.com>
2024-11-04 19:23:22 -0500

    Correccion en la automaticacion de redirecciones

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,7 +9,11 @@ export function middleware(request: NextRequest) {
   const subsecciones = getSubsecciones(pathname);
 
   // Si hay subsecciones y la ruta actual no apunta a una subsección específica
-  if (subsecciones.length > 0 && !pathname.endsWith(subsecciones[0].path)) {
+  if (
+    subsecciones.length > 0 &&
+    pathname.split("/").length < 5 &&
+    pathname.split("/")[5].length > 0
+  ) {
     // Redirige a la primera subsección
     const newUrl = `${subsecciones[0].path}`;
     return NextResponse.redirect(new URL(newUrl, request.url));
