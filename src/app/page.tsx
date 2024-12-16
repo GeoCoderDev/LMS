1c5be053f77a38fc0613d4d9ba5f114106271510
PIEROLS15 <pierodanielllanossanchez@gmail.com>
2024-12-15 19:59:27 -0500

    Header prinicipal convertido a componente

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,18 @@
 "use client";
 import Mando from "@/components/icons/Mando";
 import { Link } from "next-view-transitions";
+import Header from '@/components/Header';
 
 export default function Home() {
 
   return (
     <>
-      <header className="sticky h-[10dvh] max-h-[100px] -border-b-[1px] border-black w-screen z-[4] flex justify-center gap-10 items-center ">
-        <Link href={"/"}>
-          <span style={{ viewTransitionName: "titulo-inicio" }} className="text-[1rem] lg:text-[1.1rem] cursor-pointer">
-            Inicio
-          </span>
-        </Link>
-        <Link href={"/modulos"}>
-          <span
-            style={{ viewTransitionName: "titulo-modulos" }}
-            className="text-[1rem] lg:text-[1.1rem] cursor-pointer"
-          >
-            Modulos
-          </span>
-        </Link>
-        <Link href={"/acerca_de"}>
-          <span className="text-[1rem] lg:text-[1.1rem] cursor-pointer">
-            Acerca de
-          </span>
-        </Link>
-        <Link href={"/contacto"}>
-          <span className="text-[1rem] lg:text-[1.1rem] cursor-pointer">
-            Contacto
-          </span>
-        </Link>
-      </header>
+      <Header />
 
       <div className="w-0 h-0 absolute top-0 left-0" style={{ viewTransitionName: "modulo-splash-1" }}></div>
       <div className="w-0 h-0 absolute top-0 right-0" style={{ viewTransitionName: "modulo-splash-2" }}></div>
       <div className="w-0 h-0 absolute bottom-0 left-0" style={{ viewTransitionName: "modulo-splash-3" }}></div>
       <div className="w-0 h-0 absolute bottom-0 right-0" style={{ viewTransitionName: "modulo-splash-4" }}></div>
-
-
 
       <div className="flex flex-col w-screen h-[90dvh] items-center justify-center gap-[8vh]">
         <div className="flex flex-col items-center justify-center gap-8">
@@ -64,7 +39,7 @@ export default function Home() {
         </div>
 
         <Mando className="aspect-auto w-[16rem] md:w-[18rem]  lg:w-[20vw] animate__animated animate__rubberBand animate__infinite animate__delay-5s" />
-        
+
       </div>
 
       <style
