1df5bc840b67ff535cedf900c639e9b277846a82
PIEROLS15 <pierodanielllanossanchez@gmail.com>
2024-12-17 12:17:47 -0500

    Diseño del error 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,22 @@
+'use client';
 import React from "react";
+import Footer from "@/components/shared/Footer";
+import Header from "@/components/Header";
+import Mando from "@/components/icons/Mando";
 
 const NotFound = () => {
   return (
-    <div className="w-full max-w-screen flex items-center justify-center max-h-[100dvh] h-[100dvh]">
-      No se encontro el recurso que solicitas
-    </div>
+    <>
+      <Header />
+      <div className="flex flex-col items-center justify-center w-full h-[80dvh]">
+        <div className="text-4xl font-bold mb-4">404 - No se encontró el recurso</div>
+        <div className="text-xl text-gray-600 mb-6">
+          El mando parece estar desconectado. Intenta revisar el enlace.
+        </div>
+        <Mando className="aspect-auto w-[16rem] md:w-[18rem]  lg:w-[20vw] animate__animated animate__rubberBand animate__infinite animate__delay-5s" />
+      </div>
+      <Footer />
+    </>
   );
 };
 
